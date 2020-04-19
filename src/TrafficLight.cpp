0b6a05fbfc8bcd6d6c72bcafd2b796ba2c809f7a
encomp <edgar.martinez.rico@gmail.com>
2020-04-19 00:33:22 -0700

    Initial submission.

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -1,39 +1,49 @@
+#include <chrono>
 #include <iostream>
 #include <random>
+#include <thread>
 #include "TrafficLight.h"
 
 /* Implementation of class "MessageQueue" */
-
-/* 
 template <typename T>
 T MessageQueue<T>::receive()
 {
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
-    // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+    std::unique_lock<std::mutex> uLock(_mutex);
+    _condition.wait(uLock, [this] { return !_messages.empty(); });
+    T msg = std::move(_messages.back());
+    _messages.pop_back();
+    return msg;
 }
 
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
-    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
-    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> uLock(_mutex);
+    _messages.push_back(std::move(msg));
+    _condition.notify_one();
 }
-*/
 
 /* Implementation of class "TrafficLight" */
-
-/* 
 TrafficLight::TrafficLight()
 {
-    _currentPhase = TrafficLightPhase::red;
+    _currentPhase = TrafficLightPhase::RED;
 }
 
 void TrafficLight::waitForGreen()
 {
-    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
-    // runs and repeatedly calls the receive function on the message queue. 
-    // Once it receives TrafficLightPhase::green, the method returns.
+    while (true)
+    {
+        TrafficLightPhase trafficLightPhase = messageQueue.receive();
+        if (TrafficLightPhase::GREEN == trafficLightPhase)
+        {
+            break;
+        }
+    }
+}
+
+void TrafficLight::setCurrentPhase(TrafficLightPhase trafficLightPhase)
+{
+    _currentPhase = trafficLightPhase;
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
@@ -43,16 +53,40 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
 void TrafficLight::cycleThroughPhases()
 {
-    // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
-    // and toggles the current phase of the traffic light between red and green and sends an update method 
-    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
-}
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(4, 6);
+    int sleepFor = distribution(generator);
+    auto start = std::chrono::steady_clock::now();
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        auto end = std::chrono::steady_clock::now();
+        auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        if (sleepFor * 1000 <= elapsedTime)
+        {
+            TrafficLightPhase currentPhase = getCurrentPhase();
+            TrafficLightPhase newPhase;
+            switch (currentPhase)
+            {
+            case TrafficLightPhase::GREEN:
+                newPhase = TrafficLightPhase::RED;
+                break;
 
-*/
+            case TrafficLightPhase::RED:
+                newPhase = TrafficLightPhase::GREEN;
+                break;
+            }
+
+            setCurrentPhase(newPhase);
+            messageQueue.send(std::move(newPhase));
+            sleepFor = distribution(generator);
+            start = std::chrono::steady_clock::now();
+        }
+    }
+}
